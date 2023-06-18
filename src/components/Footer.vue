a06143356483a2b744459416a218e76716d32cec
michael <m@ciccarel.li>
2023-06-17 21:34:05 -0400

    update footer text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
   <footer class="grid grid-cols-2 md:grid-cols-3 gap-4">
     <div class="p-10">
-      <p class="mb-0">freelance developer <br />Brooklyn, New York</p>
+      <p class="mb-0">freelance developer<br />bk, ny</p>
     </div>
     <div class="p-10">
       <p class="mb-0 text-right md:text-center">
-        <a href="mailto:mikecicc@gmail.com">email</a> — <a href="https://twitter.com/mciccarelli">twitter</a> <br />
-        <a href="https://instagram.com/minorvillain">Instagram</a> — <a href="https://github.com/mciccarelli">Github</a>
+        <a href="mailto:mikecicc@gmail.com">Email</a> — <a href="https://twitter.com/mciccarelli">Twitter</a> <br />
+        <a href="https://github.com/mciccarelli">Github</a>
       </p>
     </div>
     <div class="hidden md:block p-10 md:col-span-1">
