91bb3cf57577336360931094a193b67b6a2bd62e
michael <m@ciccarel.li>
2024-03-23 15:12:36 -0400

    smooth out image blur animation

--- components/scroll-blur-image.js
+++ components/scroll-blur-image.js
@@ -1,16 +1,25 @@
 'use client';
 
 import React from 'react';
-import { motion, useAnimation } from 'framer-motion';
+import { AnimatePresence, motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
+
+const generateInlineBlurStyles = value => ({
+  WebkitBackfaceVisibility: 'hidden',
+  MozBackfaceVisibility: 'hidden',
+  WebkitTransform: 'translate3d(0, 0, 0)',
+  MozTransform: 'translate3d(0, 0, 0)',
+  WebkitFilter: `blur(${value}px)`,
+  filter: `blur(${value}px)`
+});
 
 const ScrollBlurImage = ({ imageUrl }) => {
   const controls = useAnimation();
   const { ref, inView } = useInView();
 
   const variants = {
-    visible: { filter: 'blur(0px)' },
-    hidden: { filter: 'blur(8px)' }
+    visible: { ...generateInlineBlurStyles(0) },
+    hidden: { ...generateInlineBlurStyles(8) }
   };
 
   React.useEffect(() => {
@@ -22,16 +31,18 @@ const ScrollBlurImage = ({ imageUrl }) => {
   }, [controls, inView]);
 
   return (
-    <motion.img
-      ref={ref}
-      src={imageUrl}
-      alt=""
-      initial="hidden"
-      animate={controls}
-      variants={variants}
-      transition={{ duration: 0.5 }}
-      className="w-full max-w-full h-auto mb-4 drop-shadow-none will-change-auto"
-    />
+    <AnimatePresence>
+      <motion.img
+        ref={ref}
+        src={imageUrl}
+        alt=""
+        initial="hidden"
+        animate={controls}
+        variants={variants}
+        transition={{ duration: 0.5 }}
+        className="w-full max-w-full h-auto mb-4 drop-shadow-none will-change-auto"
+      />
+    </AnimatePresence>
   );
 };
 
