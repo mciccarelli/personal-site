45e2853d2cef07ed4f55a7c2470a89100278de55
michael <mikecicc@gmail.com>
2024-10-20 23:22:56 -0400

    update social links, add accent color

--- src/components/blinking-dot.tsx
+++ src/components/blinking-dot.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion';
 export default function BlinkingDot() {
 	return (
 		<motion.div
-			className="w-1.5 h-1.5 bg-black dark:bg-white rounded-full"
+			className="w-1.5 h-1.5 bg-accent rounded-full"
 			animate={{ opacity: [1, 0, 1] }} // Blinking effect
 			transition={{
 				duration: 1, // Duration of one blink
